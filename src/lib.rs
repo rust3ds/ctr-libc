3a19713d40d2a1a996e1b971a83f1ce30f5ad8cd
Fenrir <fenrirwolf@gmail.com>
2016-06-17 19:24:04 -0700

    Added strlen function

--- src/lib.rs
+++ src/lib.rs
@@ -50,7 +50,8 @@ pub type sighandler_t = size_t;
 extern "C" {
     pub fn memchr(cx: *const c_void, c: c_int, n: size_t) -> *mut c_void;
     pub fn memrchr(cx: *const c_void, c: c_int, n: size_t) -> *mut c_void;
-    pub fn signal( signum: c_int, handler: sighandler_t) -> sighandler_t;
+    pub fn signal(signum: c_int, handler: sighandler_t) -> sighandler_t;
+    pub fn strlen(cs: *const c_char) -> size_t;
     //TODO: write() will be converted to use c types at a later time.
     //Doing so right now would break ctru-rs
     pub fn write(fd: i32, buf: *const c_void, count: usize) -> isize;
